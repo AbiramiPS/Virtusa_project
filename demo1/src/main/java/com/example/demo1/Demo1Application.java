cd70abac9cdad757e9b9bf779bfe4415803e89b0
AbiramiPS <abiabhi2712@gmail.com>
2022-11-25 17:37:30 +0530

    first commit

---
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,30 @@
+package com.example.demo1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class Demo1Application implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO details(id, name, email, password) VALUES (?, ?, ?, ?)";
+
+		int result = jdbcTemplate.update(sql, 2, "Abi", "abi@gmail.com", "abi123");
+
+		if (result > 0) {
+			System.out.println("A new row has been inserted");
+		}
+	}
+}
